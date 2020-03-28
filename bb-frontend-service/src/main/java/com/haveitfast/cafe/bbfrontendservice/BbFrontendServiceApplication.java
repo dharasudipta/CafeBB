d6d174b864731c51f162e72e464348c5871730d7
SUDIPTA DHARA <dharasudipta@gmail.com>
2020-03-28 21:38:41 +0530

    frond end service for react added

---
+++ bb-frontend-service/src/main/java/com/haveitfast/cafe/bbfrontendservice/BbFrontendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.haveitfast.cafe.bbfrontendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbFrontendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbFrontendServiceApplication.class, args);
+	}
+
+}
---
+++ bb-frontend-service/src/test/java/com/haveitfast/cafe/bbfrontendservice/BbFrontendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haveitfast.cafe.bbfrontendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbFrontendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
